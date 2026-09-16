1be950d0b5b82c3bf1b94f20ea4c8deafd7f17eb
KaiyoDev <sadboydev06@gmail.com>
2026-09-16 13:54:07 +0700

    Revert "Merge pull request #40 from KaiyoDev/FT8-49-Test-Cases&Requirements-Traceability"
    This reverts commit 6604d2bf5e8fff917d6e0342f56a7c988f7c7342, reversing
    changes made to 72efb9dd8c6bab29f342f49538f67cd0e32ef192.

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-# File: backend/database.py
-from sqlalchemy.ext.declarative import declarative_base
-
-# Tạo Base model giả
-Base = declarative_base()
-
-# Tạo hàm get_db giả
-def get_db():
-    yield None
